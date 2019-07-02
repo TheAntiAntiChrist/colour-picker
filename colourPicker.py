4875c19cb05b9717f0e4629c9c667e758ba4a99e
Oscar <36861657+TheAntiAntiChrist@users.noreply.github.com>
2019-07-02 12:02:11 +0100

    Added freeze function

--- colourPicker.py
+++ colourPicker.py
@@ -4,7 +4,6 @@ from tkinter import *
 import os
 
 #https://stackoverflow.com/questions/50487957/print-all-pygame-events-in-a-tkinter-mainloop
-#TODO: click to freeze current colour choice, then click on button to copy colour value. click colour box again to unfreeze current colour choice.
 #TODO: save up to 10 colours (and also ability to export to txt file). use 10 smaller-than-the-main buttons to show these colours. these smaller buttons should also be able to copy their colour to the clipboard when clicked.
 
 mainWindow = tk.Tk()
@@ -31,9 +30,6 @@ colourOutput.grid(column=1,columnspan=3,row=1,sticky=N)
 hexOutput = Entry(mainWindow,width=25,state="readonly")
 hexOutput.grid(column=1,columnspan=3,row=1)
 
-colourButton = Button(mainWindow,width=10,state=DISABLED)
-colourButton.grid(column=1,columnspan=3,row=0)
-
 def writeToOutput(colourData):
     colourOutput.config(state=NORMAL)
     colourOutput.delete(0,END)
@@ -58,6 +54,13 @@ def convertToHexCol(RGB):
             colour += "00"
     return colour
 
+def copyColourToCB():
+    mainWindow.clipboard_clear()
+    mainWindow.clipboard_append(hexOutput.get())
+
+colourButton = Button(mainWindow,width=10, command=copyColourToCB)#,state=DISABLED)
+colourButton.grid(column=1,columnspan=3,row=0)
+
 os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
 os.environ['SDL_VIDEODRIVER'] = 'windib'
 
@@ -66,21 +69,33 @@ screen.fill(pygame.Color(255,255,255))
 pygame.display.init()
 pygame.display.update()
 
+clock = pygame.time.Clock()
+
+isFrozen = False
+
 while True:
     redVal = redSlider.get()
     blueVal = blueSlider.get()
     greenVal = greenSlider.get()
     for event in pygame.event.get():
         if event.type == pygame.MOUSEMOTION:
-            colour = [ round(redVal*(255-(255/5000)*((event.pos[0]/10)**2+(event.pos[1]/10)**2))) , round(greenVal*((255/50)*(event.pos[0]/10))) , round(blueVal*((255/50)*(event.pos[1]/10))) ]
-            hexColour = convertToHexCol([ round(redVal*(255-(255/5000)*((event.pos[0]/10)**2+(event.pos[1]/10)**2))) , round(greenVal*((255/50)*(event.pos[0]/10))) , round(blueVal*((255/50)*(event.pos[1]/10))) ])
-            writeToOutput(colour)
-            writeToHex(hexColour)
-            colourButton.config(background=hexColour)
+            if not isFrozen:
+                colour = [ round(redVal*(255-(255/5000)*((event.pos[0]/10)**2+(event.pos[1]/10)**2))) , round(greenVal*((255/50)*(event.pos[0]/10))) , round(blueVal*((255/50)*(event.pos[1]/10))) ]
+                hexColour = convertToHexCol([ round(redVal*(255-(255/5000)*((event.pos[0]/10)**2+(event.pos[1]/10)**2))) , round(greenVal*((255/50)*(event.pos[0]/10))) , round(blueVal*((255/50)*(event.pos[1]/10))) ])
+                writeToOutput(colour)
+                writeToHex(hexColour)
+                colourButton.config(background=hexColour)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if isFrozen:
+                isFrozen = False
+            else:
+                isFrozen = True
+
 
     for x in range(0,50):
         for y in range(0,50):
-            pygame.draw.rect(screen, [redVal*(255-(255/5000)*(x**2+y**2)),greenVal*((255/50)*x),blueVal*((255/50)*y)],[x*10,y*10,10,10],0)
+            pygame.draw.rect(screen, [redVal*(255-(255/100)*(x+y)),greenVal*((255/50)*x),blueVal*((255/50)*y)],[x*10,y*10,10,10],0)
 
     pygame.display.update()
+    clock.tick(60)
     mainWindow.update()
